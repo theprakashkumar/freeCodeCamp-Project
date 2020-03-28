3a165b2446cdd937417c8f531c42e1a9075c3b68
pk <38276550+ThePrakashKumar@users.noreply.github.com>
2020-03-29 00:47:04 +0530

    Done Random Quote Machine

--- Fron End Libraries/Radom Quote Machine/app.js
+++ Fron End Libraries/Radom Quote Machine/app.js
@@ -1,7 +1,8 @@
+// For some reason on CodePen emoji icon is not working(line no 23) so over there is am using:<img src="https://emoji-css.afeld.me/emoji/v.png" id="peace"/>
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state={quote:'', by:''};
+        this.state={quote:'', by:'', loading: true};
         this.loadNewOne=this.loadNewOne.bind(this);
     }
     componentDidMount(){
@@ -9,20 +10,27 @@ class App extends React.Component{
     }
     //Get a new quote.
     async loadNewOne(){
-        const apiUrl="http://quotes.stormconsultancy.co.uk/random.json";
-        let response= await fetch(apiUrl).then(res => res.json()).then(data => data);
-        console.log(response)
-        this.setState({quote: response.quote, by: response.author})
+        const apiUrl="https://cors-anywhere.herokuapp.com/"+"https://opinionated-quotes-api.gigalixirapp.com/v1/quotes";
+        this.setState({loading: true});
+        let response= await fetch(apiUrl).then(res => res.json()).then(data => data).catch(function() {
+            console.log("error");
+        });
+        if(!response.quotes[0].author) response.quotes[0].author="Unknown";
+        this.setState({quote: response.quotes[0].quote, by: response.quotes[0].author, loading: false});
     }
     render(){
     return(
-        // User Story #1
-        <div id='quote-box'>
-            <QuoteAuthor quote={this.state.quote} author={this.state.by}/>
-            <Buttons fun={this.loadNewOne} id={'new-quote'} buttonText={'New Quote'}/>
-            <a class="twitter-share-button" href={`https://twitter.com/intent/tweet?text=${this.state.quote}`} hashtags='this'>
-                Tweet
-            </a>
+        <div>
+            {this.state.loading ? <div id="peace-container"><i className="em em-v" ariarole="presentation" aria-label="VICTORY HAND" id="peace"></i></div> : (
+                <div id='quote-box'>
+                    <QuoteAuthor quote={this.state.quote} author={this.state.by}/>
+                    <a id='tweet-quote' className="twitter-share-button" href={`https://twitter.com/intent/tweet?text=${this.state.quote}&hashtags=quote`}>
+                        <i className="fab fa-twitter fa-lg"></i>
+                    </a>
+                    <a href={`https://api.whatsapp.com/send?text=${this.state.quote}`}><i className="fab fa-whatsapp fa-lg"></i></a>
+                    <button id='new-quote' onClick={this.loadNewOne}>New Quote</button>
+                </div>
+            )}
         </div>
     )
     }
@@ -32,25 +40,9 @@ const QuoteAuthor=(props) => {
         return(
             <div>
                 <p id='text'>{props.quote}</p>
-                <br/>
                 <p id='author'>{`- ${props.author}`}</p>
             </div>
         )
-}
-//Component to render button
-class Buttons extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleClick=this.handleClick.bind(this);
-    }
-    handleClick(){
-        this.props.fun();
-    }
-    render(){
-        return(
-            <button id={this.props.id} onClick={this.handleClick}>{this.props.buttonText}</button>
-        )
-    }
 }
 
 ReactDOM.render(<App/>, document.getElementById("root"));
