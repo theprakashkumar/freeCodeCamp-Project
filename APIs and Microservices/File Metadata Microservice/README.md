844ddae3c700c2f65d4f5b474aff19f21ce72f54
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2020-08-14 11:39:17 +0530

    done file metadata project

---
+++ APIs and Microservices/File Metadata Microservice/README.md
@@ -0,0 +1,7 @@
+
+# API Project: File Metadata Microservice for freeCodeCamp
+
+####    User stories:
+1. I can submit a form that includes a file upload.
+2. The form file input field  has the "name" attribute set to "upfile". We rely on this in testing.
+3. When I submit something, I will receive the file name and size in bytes within the JSON response
---
+++ APIs and Microservices/File Metadata Microservice/server.js
@@ -0,0 +1,33 @@
+// <input id="inputfield" type="file" name="upfile">
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+var multer=require('multer');
+
+//Setup & Middleware
+var app = express();
+var storage=multer.memoryStorage();
+var upload=multer({storage: storage});
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
